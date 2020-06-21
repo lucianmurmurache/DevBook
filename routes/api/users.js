5acf20129f0f550d0c1bf6f4a9cfa6a66e0b9f54
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-06-21 22:30:45 +0200

    Added routes
    Added auth, posts, profile and users routes

---
+++ routes/api/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/users
+// @desc    Test route
+// @access  Public
+// router.get('/', (req, res) => res.send('User route'));
+
+// @route   POST api/users
+// @desc    Test route
+// @access  Public
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('User route');
+});
+
+module.exports = router;
