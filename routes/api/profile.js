1d6aecef9797c7a67f199cb2e3d3811cb0070695
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-07-04 17:07:10 +0200

    Fixes and new routes
    Fixed github username get request
    Created Post model
    Added post routes
    -

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
 const config = require('config');
+const axios = require('axios');
 
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
@@ -299,29 +299,20 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
  */
 router.get('/github/:username', async (req, res) => {
     try {
-        const options = {
-            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5%sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
-            method: 'GET',
-            headers: {
-                'user-agent': 'node.js',
-                // =====  Currently not returning gh-profile = work in progress =====
-            }
+        const uri = encodeURI(
+            `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+        );
+        const headers = {
+            'user-agent': 'node.js',
+            Authorization: `token ${config.get('githubToken')}`
         };
-        request(options, (error, response, body) => {
-            if (error) console.error(error);
-            if (response.statusCode !== 200) {
-                res.status(400).json({
-                    msg: 'No github profile found'
-                });
-            }
-            res.json(JSON.parse(body));
-        });
+
+        const gitHubResponse = await axios.get(uri, { headers });
+        return res.json(gitHubResponse.data);
     } catch (err) {
         console.error(err.message);
-        return res.status(500).json({ msg: 'Server Error' });
+        return res.status(404).json({ msg: 'No Github profile found' });
     }
 });
-
-
 
 module.exports = router;
