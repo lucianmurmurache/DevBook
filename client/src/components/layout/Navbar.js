032776b4794a9593ffa9d6057608802cfbb8b6ce
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-07-10 18:26:11 +0200

    Updated authLinks and guestLinks
    Added link to profiles for authLinks
    Added link to profiles for guestLinks

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,13 +1,18 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
     const authLinks = (
         <ul>
+            <li>
+                <Link to='/profiles'>
+                    Developers
+                </Link>
+            </li>
             <li>
                 <Link to='/dashboard'>
                     <i className='fas fa-user' />{' '}
@@ -19,16 +24,30 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
             <li>
                 <a onClick={logout} href='#!'>
                     <i className='fas fa-sign-out-alt' />{' '}
-                    <span className='hide-sm'>Logout</span>
+                    <span className='hide-sm'>
+                        Logout
+                    </span>
                 </a>
             </li>
         </ul>
     );
     const guestLinks = (
         <ul>
-            <li><Link to='#!'>Developers</Link></li>
-            <li><Link to='/register'>Register</Link></li>
-            <li><Link to='/login'>Login</Link></li>
+            <li>
+                <Link to='/profiles'>
+                    Developers
+                </Link>
+            </li>
+            <li>
+                <Link to='/register'>
+                    Register
+                </Link>
+            </li>
+            <li>
+                <Link to='/login'>
+                    Login
+                </Link>
+            </li>
         </ul>
     );
     return (
