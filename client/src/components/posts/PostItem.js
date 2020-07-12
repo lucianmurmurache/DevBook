c447a838dc5d0a4ee82696a127f1150f27340c71
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-07-12 16:32:42 +0200

    Added deletePost functionality

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike } from '../../actions/post';
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
 const PostItem = ({
     auth,
     addLike,
     removeLike,
+    deletePost,
     post: {
         _id,
         text,
@@ -51,7 +52,10 @@ const PostItem = ({
                 >
                     <i className='fas fa-thumbs-down' />
                 </button>
-                <Link to={`/posts/${_id}`} className='btn btn-primary'>
+                <Link
+                    to={`/posts/${_id}`}
+                    className='btn btn-primary'
+                >
                     Discussion{' '}{comments.length > 0 && (
                         <span className='comment-count'>{comments.length}</span>
                     )}
@@ -60,11 +64,11 @@ const PostItem = ({
                     <button
                         type='button'
                         className='btn btn-danger'
+                        onClick={() => deletePost(_id)}
                     >
-                        <i class='fas fa-trash' />
+                        <i className='fas fa-trash' />
                     </button>
                 )}
-
             </div>
         </div>
     )
@@ -72,10 +76,13 @@ const PostItem = ({
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(PostItem);
