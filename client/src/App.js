7f44e0685f368b98f6cd9b18d6137b73fe59f385
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-07-12 16:03:20 +0200

    Added posts and post like/unlike functionality

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -45,9 +46,10 @@ const App = () => {
               <Route exact path='/profile/:id' component={Profile} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={ProfileForm} />
-              <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
-              <PrivateRoute exact path="/add-experience" component={AddExperience} />
-              <PrivateRoute exact path="/add-education" component={AddEducation} />
+              <PrivateRoute exact path='/edit-profile' component={ProfileForm} />
+              <PrivateRoute exact path='/add-experience' component={AddExperience} />
+              <PrivateRoute exact path='/add-education' component={AddEducation} />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
