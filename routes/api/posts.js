8efa5144a3a8e5b1b33ddbef4848bbd4eba3b5c8
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-07-12 20:20:55 +0200

    Added return to #135
    Also removed some empty spaces

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,7 +4,6 @@ const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
 const Post = require('../../models/Post');
-const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 /**
@@ -15,13 +14,10 @@ const User = require('../../models/User');
 router.post('/', [auth, [
     check('text', 'Text is required').not().isEmpty()
 ]], async (req, res) => {
-
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-
     try {
         const user = await User.findById(req.user.id).select('-password');
         const newPost = new Post({
@@ -85,7 +81,6 @@ router.delete('/:id', auth, async (req, res) => {
         if (!post) { // Check post
             return res.status(404).json({ msg: 'Post not found' });
         }
-
         if (post.user.toString() !== req.user.id) { // Check user
             return res.status(401).json({ msg: 'User not authorised' });
         }
@@ -108,7 +103,6 @@ router.delete('/:id', auth, async (req, res) => {
 router.put('/like/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-
         // Check if post has been liked by user
         if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
             return res.status(400).json({ msg: 'Post already liked' });
@@ -130,7 +124,6 @@ router.put('/like/:id', auth, async (req, res) => {
 router.put('/unlike/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-
         // Check if post has been liked by user
         if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
             return res.status(400).json({ msg: 'Post has not been liked' });
@@ -139,7 +132,7 @@ router.put('/unlike/:id', auth, async (req, res) => {
         const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
         post.likes.splice(removeIndex, 1);
         await post.save();
-        res.json(post.likes);
+        return res.json(post.likes);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
@@ -154,16 +147,13 @@ router.put('/unlike/:id', auth, async (req, res) => {
 router.post('/comment/:id', [auth, [
     check('text', 'Text is required').not().isEmpty()
 ]], async (req, res) => {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-
     try {
         const user = await User.findById(req.user.id).select('-password');
         const post = await Post.findById(req.params.id);
-
         const newComment = {
             text: req.body.text,
             name: user.name,
